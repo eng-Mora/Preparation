34870d0b9fff96aeaa287cf154c3b4107d08fe6e
Amro Mohamad <mamro2526@gmail.com>
2026-03-19 02:46:17 +0200

    Fix missing newline at end of script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getDatabase, ref, get, push, set, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
